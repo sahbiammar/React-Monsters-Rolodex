f424c84f4bc85d47a76861e89d72cf3cc946b61b
sahbiammar <sahbi.ammar.1995@gmail.com>
2020-09-13 23:32:32 +0200

    Updates

---
+++ precache-manifest.94fd983ffca34e5cbf98e5ca389c6a38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7315173079765131e3df422711c2ff8",
+    "url": "/React-Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "6feaa76b610f76c5adf4",
+    "url": "/React-Monsters-Rolodex/static/css/main.8539385c.chunk.css"
+  },
+  {
+    "revision": "cbe8bf7a8c21d33a73ea",
+    "url": "/React-Monsters-Rolodex/static/js/2.f3fec2b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Monsters-Rolodex/static/js/2.f3fec2b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6feaa76b610f76c5adf4",
+    "url": "/React-Monsters-Rolodex/static/js/main.7e003f14.chunk.js"
+  },
+  {
+    "revision": "32ebeb5f077ed04b2675",
+    "url": "/React-Monsters-Rolodex/static/js/runtime-main.6ec4681b.js"
+  }
+]);
